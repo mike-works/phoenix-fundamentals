53022a74b8c9a9fb4a326f8d6b17e02307f2c0bf
Mike North <michael.l.north@gmail.com>
2017-04-26 11:07:52 +0200

    exercise 1 - mandatory queryparam

--- web/plugs/mike.ex
+++ web/plugs/mike.ex
@@ -1,8 +1,14 @@
 defmodule Mike do
   import Plug.Conn
   def init(opts), do: opts
+  
+  def call(conn = %{params: %{"must_have" => _}}, _opts), do: conn
+
+  # We need to reject this  
   def call(conn, _opts) do
-    IO.inspect conn.params
+
     conn
+    |> resp(422, "You're missing the required queryparam")
+    |> halt
   end
 end
